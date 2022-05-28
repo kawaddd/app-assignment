935aa49496c3bf253572f970a5d1785ef79288b5
kawaddd <104880538+kawaddd@users.noreply.github.com>
2022-05-28 13:45:25 +0000

    main

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,27 @@
 class BooksController < ApplicationController
 
   def index
+    @book=Book.new
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @book.save
+    # 4. 作成した本の詳細画面へリダイレクト
+    redirect_to book_path(@book.id)
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end

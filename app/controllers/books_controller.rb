4e0b96eff0d74b2c98fb831eb2d420f66685d61d
kawaddd <104880538+kawaddd@users.noreply.github.com>
2022-05-29 10:56:18 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,16 @@
 class BooksController < ApplicationController
 
   def index
+    @books=Book.all
     @book=Book.new
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+    @book = Book.find(params[:id])  
   end
   
   def create

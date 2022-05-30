b5926a895e99930e1aedf81f0eab39af7ebd7ef7
kawaddd <104880538+kawaddd@users.noreply.github.com>
2022-05-30 13:25:38 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,12 @@ class BooksController < ApplicationController
     redirect_to book_path(@book.id)
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
   private
   # ストロングパラメータ
   def book_params

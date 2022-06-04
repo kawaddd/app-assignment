571708b29ae49bcae4a114d01ba4f3366bf29daa
kawaddd <104880538+kawaddd@users.noreply.github.com>
2022-06-04 04:50:11 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,18 +14,30 @@ class BooksController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @books=Book.all
     @book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
-    # 4. 作成した本の詳細画面へリダイレクト
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id) 
+    else
+      render :index
+    end
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)  
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  
+    book.destroy  
+    redirect_to '/books'  
   end
   
   private
